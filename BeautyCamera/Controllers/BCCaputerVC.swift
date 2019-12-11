d022d695519a4dfe50559959ea2a9f8429fabdec
李嘉翔 <jiaxiang@dailyinnovation.biz>
2019-12-11 17:56:08 +0800

    [feat]初步建立项目。

---
+++ BeautyCamera/Controllers/BCCaputerVC.swift
@@ -0,0 +1,41 @@
+//
+//  BCCaputerVC.swift
+//  BeautyCamera
+//
+//  Created by Jiaxiang on 2019/12/11.
+//  Copyright © 2019 Jiaxiang. All rights reserved.
+//
+
+import UIKit
+import GPUImage
+import SnapKit
+
+class BCCaputerVC: UIViewController {
+    
+    lazy var captureView: UIView = {() -> UIView in
+        let view = UIView.init()
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.initConfiguration()
+        self.setupViews()
+    }
+    
+    func initConfiguration() {
+        
+    }
+    
+    func setupViews() {
+        self.view.addSubview(captureView)
+        captureView.snp.makeConstraints { (maker) in
+            maker.edges.equalToSuperview()
+        }
+    }
+
+
+}
